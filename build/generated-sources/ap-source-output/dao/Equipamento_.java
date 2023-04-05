f9380fd8b90431ac35b2f3d779813cbd4194325d
Marcos Vinicius <marcos.mdl44@gmail.com>
2023-04-04 22:42:03 -0300

    Finalizado

---
+++ build/generated-sources/ap-source-output/dao/Equipamento_.java
@@ -0,0 +1,17 @@
+package dao;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-04-04T14:05:13")
+@StaticMetamodel(Equipamento.class)
+public class Equipamento_ { 
+
+    public static volatile SingularAttribute<Equipamento, String> funFk;
+    public static volatile SingularAttribute<Equipamento, String> tipo;
+    public static volatile SingularAttribute<Equipamento, String> description;
+    public static volatile SingularAttribute<Equipamento, Long> id;
+    public static volatile SingularAttribute<Equipamento, String> patrimony;
+
+}
